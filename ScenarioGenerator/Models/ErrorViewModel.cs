40577cf5a41c46768f63f2477ab829700d1d6549
Daniel Vo <dan@daniel-vo.com>
2020-07-10 22:23:03 -0500

    init with dotnet mvc empty project

---
+++ ScenarioGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScenarioGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
