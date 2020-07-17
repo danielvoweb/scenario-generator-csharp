58c62f8408071df2d899f0ffcf09be88f37b6d94
Daniel Vo <dan@daniel-vo.com>
2020-07-16 21:01:28 -0500

    Removed UnitTest and replaced with NUnitTests
    Renamed test solution to focus on variations of testing for practice
    Plans to add xunit next

--- ScenarioGenerator.NUnitTests/ScenarioBuilderTests.cs
+++ ScenarioGenerator.NUnitTests/ScenarioBuilderTests.cs
@@ -1,8 +1,7 @@
 using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 
-namespace ScenarioGenerator.Tests
+namespace ScenarioGenerator.NUnitTests
 {
     public class Tests
     {
